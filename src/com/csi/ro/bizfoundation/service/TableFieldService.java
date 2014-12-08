3672f5d3b99c449bf8b890216f6c3a0f0ba4e50f
zhangkaixiang <zhangkaixiang@kokozu.net>
2014-12-08 20:02:54 +0800

    Signed-off-by: zhangkaixiang <zhangkaixiang@kokozu.net>

--- src/com/csi/ro/bizfoundation/service/TableFieldService.java
+++ src/com/csi/ro/bizfoundation/service/TableFieldService.java
@@ -5,6 +5,8 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import com.csi.ro.bizfoundation.uiengine.CreateDaoBean;
+
 public class TableFieldService {
 	private static Map<String,String> map=new HashMap<String,String>();
 	private static Map<String,String> maptype=new HashMap<String,String>();
@@ -17,10 +19,13 @@ public class TableFieldService {
 		maptype.put("varchar", "String");
 		maptype.put("datetime", "Date");
 		maptype.put("date", "Date");
-		maptype.put("float", "float");
-		maptype.put("tinyint", "int");
-		maptype.put("int", "int");
+		maptype.put("float", "Float");
+		maptype.put("tinyint", "Integer");
+		maptype.put("int", "Long");
+		maptype.put("bigint", "Long");
 		maptype.put("time", "Date");
+		maptype.put("text", "String");
+		maptype.put("smallint", "Integer");
 		
 		mapjson.put("datetime", "@JsonFormat(pattern = \"yyyy-MM-dd HH:mm:ss\", timezone = \"GMT+08:00\")");
 		mapjson.put("date", "@JsonFormat(pattern = \"yyyy-MM-dd\", timezone = \"GMT+08:00\")");
@@ -87,6 +92,30 @@ public class TableFieldService {
 			}
 		}
 		return "";
+	}
+	
+	public String tableNames(){
+		return CreateDaoBean.currentTableName();
+	}
+	
+	public String tableNamess(){
+		StringBuilder sb=new StringBuilder();
+		
+		String tname = CreateDaoBean.currentTableName();
+		
+		if(tname!=null && !"".equals(tname)){
+			List<String> list = Arrays.asList(tname.split("_"));
+			for(int i=0;i<list.size();i++){
+				String s=list.get(i);
+				String ups=s.substring(0, 1).toUpperCase()+s.substring(1, s.length()).toLowerCase();
+				sb.append(ups);
+			}
+		}
+		return sb.toString();
+	}
+	
+	public String packagePath(){
+		return CreateDaoBean.packagePath();
 	}
 	
 	
--- src/com/csi/ro/bizfoundation/uiengine/CreateDaoBean.java
+++ src/com/csi/ro/bizfoundation/uiengine/CreateDaoBean.java
@@ -1,14 +1,36 @@
 package com.csi.ro.bizfoundation.uiengine;
 
+import com.csi.ro.bizfoundation.service.TableFieldService;
+
 public class CreateDaoBean {
+	
+	public static String currentTableName(){
+		return "actfix_activity";
+	}
+	public static String packagePath(){
+		return "com.ariadnethread.activity.actfix";
+	}
 
 	 public static void main(String[] args) {
-		 String xmlPath="D:\\workSpace\\movie-service\\movie-redactivity\\src\\main\\java\\com\\ariadnethread\\redactivity\\rulemanager\\dao\\mybatis";
-		 String beanPath="D:\\workSpace\\movie-service\\movie-redactivity\\src\\main\\java\\com\\ariadnethread\\redactivity\\rulemanager\\domain";
-		 String tableObj="RedPay";
+		 
+		 String xmlPath="D:\\workSpace\\movie-service\\movie-activity\\src\\main\\java\\com\\ariadnethread\\activity\\actfix\\dao\\mybatis";
+		 String beanPath="D:\\workSpace\\movie-service\\movie-activity\\src\\main\\java\\com\\ariadnethread\\activity\\actfix\\domain";
+//		 String xmlPath="D:\\workSpace\\movie-service\\movie-couponcard\\src\\main\\java\\com\\ariadnethread\\couponcard\\coupon\\dao\\mybatis";
+//		 String beanPath="D:\\workSpace\\movie-service\\movie-couponcard\\src\\main\\java\\com\\ariadnethread\\couponcard\\coupon\\domain";
+//		 String xmlPath="D:\\workSpace\\movie-service\\movie-couponcard\\src\\main\\java\\com\\ariadnethread\\couponcard\\membership\\dao\\mybatis";
+//		 String beanPath="D:\\workSpace\\movie-service\\movie-couponcard\\src\\main\\java\\com\\ariadnethread\\couponcard\\membership\\domain";
+//		 String xmlPath="D:\\workSpace\\movie-service\\movie-devicemanage\\src\\main\\java\\com\\ariadnethread\\nocmanage\\dao\\mybatis";
+//		 String beanPath="D:\\workSpace\\movie-service\\movie-devicemanage\\src\\main\\java\\com\\ariadnethread\\nocmanage\\domain";
+//		 String xmlPath="D:\\workSpace\\movie-service\\movie-redactivity\\src\\main\\java\\com\\ariadnethread\\redactivity\\ruleuser\\dao\\mybatis";
+//		 String beanPath="D:\\workSpace\\movie-service\\movie-redactivity\\src\\main\\java\\com\\ariadnethread\\redactivity\\ruleuser\\domain";
+		 
+//		 String tableObj="recharge_record_01";
+		 String tableObj=new TableFieldService().tableName(currentTableName());
+		 
 //		 createBean(beanPath,tableObj);   
-//		 createDaoXML(xmlPath,tableObj);
-		 createDao(xmlPath,tableObj);
+//		 createDao(xmlPath,tableObj);
+		 createDaoXML(xmlPath,tableObj);
+		 
 	 }
 	 
 	 /**
@@ -16,7 +38,7 @@ public class CreateDaoBean {
 	  */
     public static void createDaoXML(String destPath,String tableObj){
     	UIEngine en=new UIEngine();
-       en.uiEngine("D:\\workSpace\\velocity-uiengine\\pdm\\movie.pdm",destPath,
+       en.uiEngine("D:\\workSpace\\velocity-uiengine\\pdm\\movie-1.6v.pdm",destPath,
     		   "daoenginetemplate/engine.vm",tableObj+"Mapper.xml");
     }
     
@@ -25,7 +47,7 @@ public class CreateDaoBean {
      */
     public static void createBean(String destPath,String tableObj){
     	UIEngine en=new UIEngine();
-       en.uiEngine("D:\\workSpace\\velocity-uiengine\\pdm\\movie.pdm",destPath,
+       en.uiEngine("D:\\workSpace\\velocity-uiengine\\pdm\\movie-1.6v.pdm",destPath,
     		   "daoenginetemplate/engine_bean.vm",tableObj+".java");
     }
     
@@ -34,7 +56,7 @@ public class CreateDaoBean {
      */
     public static void createDao(String destPath,String tableObj){
     	UIEngine en=new UIEngine();
-    	en.uiEngine("D:\\workSpace\\velocity-uiengine\\pdm\\movie.pdm",destPath,
+    	en.uiEngine("D:\\workSpace\\velocity-uiengine\\pdm\\movie-1.6v.pdm",destPath,
     			"daoenginetemplate/engine_dao.vm",tableObj+"Mapper.java");
     }
 	
